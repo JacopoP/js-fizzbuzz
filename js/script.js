8f2ace3748dba08955009aac21024374a8e72490
Jac22 <jacopo.prudentino@gmail.com>
2022-11-09 13:52:00 +0100

    Completato esercizio

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const container = document.getElementById("container");
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 15 === 0){
+        container.innerHTML +=`<div class="box fizzbuzz"> fizzbuzz </div>`;
+    }
+
+    else if (i % 5 === 0){
+        container.innerHTML +=`<div class="box buzz"> buzz </div>`;
+    }
+
+    else if (i % 3 === 0){
+        container.innerHTML +=`<div class="box fizz"> fizz </div>`;
+    }
+
+    else{
+        container.innerHTML +=`<div class="box none"> ${i} </div>`;
+    }
+    
+}
